16a832a386756fee65e0ebef65d1004311370075
niranjanrajesh02 <niranjanrajesh02@gmail.com>
2024-06-26 05:17:43 +0700

    new starters

---
+++ adv_train.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#SBATCH -p gpu_v100_2
+#SBATCH -N 1
+#SBATCH -n 1
+#SBATCH --mem 20G
+#SBATCH -t 0-23:59 # time (D-HH:MM)
+#SBATCH --job-name="robCAM_train"
+#SBATCH -o out.log
+#SBATCH -e err.log
+#SBATCH --gres=gpu:1
+srun python /home/venkat/niranjan/robust_CAMs/E0.py --adv_train
