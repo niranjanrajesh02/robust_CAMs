bbb42634b4989a9bc5b26808e7b4c774441094d0
niranjanrajesh02 <niranjanrajesh02@gmail.com>
2024-06-27 21:35:41 +0700

    job script

---
+++ cam_dims.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#SBATCH -p gpu_v100_2
+#SBATCH -N 1
+#SBATCH -n 1
+#SBATCH --mem 20G
+#SBATCH -t 1-23:59 # time (D-HH:MM)
+#SBATCH --job-name="robCAM_acts"
+#SBATCH -o ev_out.log
+#SBATCH -e ev_err.log
+#SBATCH --gres=gpu:1
+srun python /home/venkat/niranjan/robust_CAMs/class_acts.py
