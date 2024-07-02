5d8584b6f665e5caa096d4d7fd507a3b33b91a6d
niranjanrajesh02 <niranjanrajesh02@gmail.com>
2024-07-02 21:51:41 +0700

    gpu pca

--- cam_dims.sh
+++ cam_dims.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-#SBATCH -p bigcompute
+#SBATCH -p gpu_a100_8
 #SBATCH -N 1
 #SBATCH -n 1
-#SBATCH --mem 100G
+#SBATCH --mem 20G
 #SBATCH -t 0-23:59 # time (D-HH:MM)
 #SBATCH --job-name="robCAM_acts"
 #SBATCH -o cam_out.log
 #SBATCH -e cam_err.log
-#SBATCH --cpus-per-task=100
+#SBATCH --gres=gpu:1
 
 
 srun python /home/venkat/niranjan/robust_CAMs/class_acts.py --dataset imagenet --arch resnet --model_type standard --task dims
